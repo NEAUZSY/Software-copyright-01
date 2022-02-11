7852f64208d788a191f93fe71714bac90aa67cbd
root <tsszsy@163.com>
2022-02-11 11:30:05 +0800

    抓取数据完成

--- request.py
+++ request.py
@@ -1,30 +1,40 @@
-"""
-基于正则re模块解析数据
-"""
-import re
 import os
 from lxml import etree
+import time
 import requests
+from utils.header import get_headers
 
-from utils.header import get_ua, get_headers
-
-URL = 'http://q.10jqka.com.cn/'
+BASE_URL = 'http://q.10jqka.com.cn/index/index/board/all/field/zdf/order/desc/page/{}/'
 
 
-def get(url):
+def get(base_url):
     # 获取请求头
     header = get_headers()
     # 判断本地是否已经存在之前爬取的网页
-    if os.path.exists('store.html'):
+    html = ''
+    if os.path.exists('stock.html'):
         # 如果已经存在之前爬取的网页便将其赋值给html
-        with open('store.html') as f:
+        with open('stock.html', encoding='GBK') as f:
             html = f.read()
     else:
         # 否则在网络上进行爬取，并存储在html变量中
-        resp = requests.get(url, headers=header)
-        assert resp.status_code == 200
-        html = resp.text
-        with open('store.html', 'w') as f:
+        for i in range(1, 21):
+            # 获取真正的url
+            url = base_url.format(i)
+
+            # 进行请求
+            resp = requests.get(url, headers=header)
+            # print(resp.status_cod)
+            # assert resp.status_code == 200
+
+            # 获取请求内容
+            html += resp.text
+
+            # 暂停一会反爬
+            print('完成了{}页抓取'.format(i))
+            time.sleep(0.1)
+
+        with open('stock.html', 'w', encoding='GBK') as f:
             f.write(html)
     # 返回网页文件
     return html
@@ -35,17 +45,24 @@ def parse(html):
     root = etree.HTML(html)
     # 找到今日个股行情信息
     tbody = root.xpath('//table/tbody/tr')
+    data = []
     for tr in tbody:
-        ID = tr.xpath('./td[1]/text()')[0]
-        print(ID)
-
-
-    compile_ = re.compile(r'<img data-src="(.*)" alt="(.*)" class')
+        # 初始化每一行的列表
+        row = [str(tr.xpath('./td[1]/text()')[0]),
+               str(tr.xpath('./td[2]/a/text()')[0]),
+               str(tr.xpath('./td[3]/a/text()')[0])]
+        for i in range(4, 15):
+            target = r'./td[{}]/text()'.format(i)
+            row.append(str(tr.xpath(target)[0]))
+        data.append(row)
+    # print(data)
+    return data
 
 
 def main():
-    html = get(URL)
-    parse(html)
+    html = get(BASE_URL)
+    data = parse(html)
+    print(data)
 
 
 if __name__ == '__main__':
